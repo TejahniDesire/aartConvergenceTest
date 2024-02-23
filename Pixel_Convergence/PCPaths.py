0585ce889008d21f2258ef8439a27a18f8e114cf
Tejahni Desire <td6241@princeton.edu>
2024-02-23 11:37:12 -0500

    Updates

--- Pixel_Convergence/PCPaths.py
+++ Pixel_Convergence/PCPaths.py
@@ -1,4 +1,8 @@
+aartPath = '/home/td6241/repositories/aart_convergence/aart_tdwork'
+import sys
+sys.path.append(aartPath)
 import paths
+
 
 aartPath = paths.aartPath
 aartPathResults = paths.aartPathResults
