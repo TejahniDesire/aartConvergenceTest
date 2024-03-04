c698b888665ae9f7573a512ada216167a96aec7d
Tejahni Desire <td6241@della8.princeton.edu>
2024-03-03 22:28:10 -0500

    test

--- EZPaths.py
+++
@@ -1,3 +0,0 @@
-aartPath = '/home/td6241/repositories/aartConvergenceTest'
-aartPathResults = '/scratch/gpfs/td6241/convergence_data/'
-aartOutput = aartPathResults + 'results/'
--- Pixel_Convergence/PCPaths.py
+++
@@ -1,9 +0,0 @@
-aartPath = '/home/td6241/repositories/aartConvergenceTest'
-import sys
-sys.path.append(aartPath)
-import EZPaths
-
-
-aartPath = EZPaths.aartPath
-aartPathResults = EZPaths.aartPathResults
-aartOutput = EZPaths.aartOutput
