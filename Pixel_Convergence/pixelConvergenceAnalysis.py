f9a27e6657246d5fc12a5c89fd123b2aad9ba999
Tejahni Desire <td6241@princeton.edu>
2024-05-02 21:18:17 -0400

    Updates

--- Pixel_Convergence/pixelConvergenceAnalysis.py
+++ Pixel_Convergence/pixelConvergenceAnalysis.py
@@ -84,7 +84,7 @@ n2_color = "#009e73"
 cumu_color = "#cc79a7"
 
 RingStyle = {
-    "label": ['n=0', 'n=1', 'n=2', 'total'],
+    "label": ['n=0', 'n=1', 'n=2', 'Total'],
     "color": [n0_color, n1_color, n2_color, cumu_color],
     "linewidth": [3, 3, 3, 4]
 }
